b4fc5d8165b05e38f161ddb3f850b55d1511ba05
utkubilge <bahautkubilge@gmail.com>
2022-01-06 03:05:54 +0300

    todo appified

--- src/index.js
+++ src/index.js
@@ -1,19 +1,76 @@
-import React from 'react';
+
+import './App.css';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-function App() {
+function TodoForm({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
   return (
-    <div style={styles.appdiv}>
-      <h1>Welcome to Pecker</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
   );
 }
 
-const styles = {
-  appdiv: {
-    textAlign: "center",
-    fontFamily: "Arial, Helvetica, sans-serif"
-  }
+function Todo({ todo, index, removeTodo }) {
+  return (
+    <div className="todo">
+      {todo.text}
+      <button onClick={() => removeTodo(index)}>x</button>
+    </div>
+    
+  );
+};
+
+
+function App() {
+  const [todos, setTodos] = React.useState([
+    { text: "Learn about React" },
+    { text: "Meet friend for lunch" },
+    { text: "Build really cool todo app" }
+  ]);
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  
+  
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  )
 }
 
 ReactDOM.render(
